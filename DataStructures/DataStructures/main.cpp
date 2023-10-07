9d66f00a12d71442512e56a4a617f2e802e66302
senthil <catchsenthil.k@gmail.com>
2023-10-07 09:13:05 -0700

    Initial checkin of project for practicing Data Structures in C++

---
+++ DataStructures/DataStructures/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStructures
+//
+//  Created by Senthil on 06/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
